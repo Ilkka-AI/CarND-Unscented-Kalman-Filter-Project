1afd28556ae0c773518a573d04653ffd594891c7
Ilkka-AI <ilkka.huopaniemi@gmail.com>
2017-11-01 19:59:56 +0100

    Initialization fixed

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -34,11 +34,11 @@ UKF::UKF() {
   is_initialized_= false;
   // Process noise standard deviation longitudinal acceleration in m/s^2
 
-  std_a_ = 0.4;
+  std_a_ = 2;//0.4;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
   
-  std_yawdd_ = 0.2;
+  std_yawdd_ = 0.8 ;//0.2;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -104,14 +104,17 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   }
    weights_=weights;
    
-   x_ << EPS,EPS,0,0,0;
-   P_<< 1,0,0,0,0,
-       0,1,0,0,0,
-       0,0,1,0,0,
-       0,0,0,1,0,
-       0,0,0,0,1; 
-   
- 
+   x_ << 0,0,0,0,0;
+   P_<< MatrixXd::Identity(5,5);
+
+ if(meas_package.sensor_type_ == MeasurementPackage::RADAR & use_radar_==true){
+ x_[0]=meas_package.raw_measurements_[0]*cos(meas_package.raw_measurements_[1]);
+    x_[1]	=meas_package.raw_measurements_[0]*sin(meas_package.raw_measurements_[1]);
+}
+if(meas_package.sensor_type_ == MeasurementPackage::LASER & use_radar_==true){
+x_[0]=meas_package.raw_measurements_[0];
+x_[1]=meas_package.raw_measurements_[1];
+}
 }
   
  else if(meas_package.sensor_type_ == MeasurementPackage::RADAR & use_radar_==true){
@@ -173,10 +176,11 @@ void UKF::Prediction(double delta_t) {
 
   //create augmented sigma points
   Xsig_aug.col(0)  = x_aug;
+  float sqrt_lambda_plus_n_aug=sqrt(lambda_+n_aug_);
   for (int i = 0; i< n_aug_; i++)
   {
-    Xsig_aug.col(i+1)       = x_aug + sqrt(lambda_+n_aug_) * L.col(i);
-    Xsig_aug.col(i+1+n_aug_) = x_aug - sqrt(lambda_+n_aug_) * L.col(i);
+    Xsig_aug.col(i+1)       = x_aug + sqrt_lambda_plus_n_aug * L.col(i);
+    Xsig_aug.col(i+1+n_aug_) = x_aug - sqrt_lambda_plus_n_aug * L.col(i);
   }
  
   //std::cout << "Xsig_aug = " << std::endl << Xsig_aug << std::endl;
